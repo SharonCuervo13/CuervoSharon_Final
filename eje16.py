be805e5d4cecb7e290e9f55987a141d596394469
SharonCuervo13 <56890913+SharonCuervo13@users.noreply.github.com>
2019-12-04 12:33:08 -0500

    ej 16

---
+++ eje16.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
