ab70246ae07c56abc297949a76f89d22c967ab84
SharonCuervo13 <56890913+SharonCuervo13@users.noreply.github.com>
2019-12-04 12:32:49 -0500

    ej15

---
+++ ej15.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[2]:
+
+
+n_puntos=10**5
+xk = np.loadtxt('valores.txt')
+sigma=1.0
+
+
+# In[3]:
+
+
+def probabilidad(xk,sigma):
+    prob=(1.0/sigma*np.sqrt(2.0*np.pi()))*np.exp((-1.0/2.0)*((xk**2.0)/(sigma**2)))
+    return prob
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[15]:
+
+
+plt.figure()
+plt.legend()
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.title("valor medio= y desviación estandar=")
+plt.savefig("sigma.png")
+
+
+# In[ ]:
+
+
+
+
